a25ef75f9a9ccc4f6c36f5aac947c8201ec986e9
Dhairya <dhairyaparikh1002@gmail.com>
2023-06-09 19:50:07 +0530

    Completed project for vite+react

---
+++ src/Components/index.js
@@ -0,0 +1,27 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Bussiness from './Bussiness';
+import CardDeal from './CardDeal';
+import Clients from './Clients';
+import CTA from './CTA';
+import FeedbackCard from './FeedbackCard';
+import Footer from './Footer';
+import GetStarted from './GetStarted';
+import Hero from './Hero';
+import Testimonials from './Testimonials';
+import Stats from './Stats';
+
+export {
+    Navbar,
+    Billing,
+    Bussiness,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedbackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Testimonials,
+    Stats
+}
